3a35087fb2f02e0f5caea4226f22a7e21641b522
joyce677 <112773487+joyce677@users.noreply.github.com>
2025-09-02 17:27:17 +0800

    Update page.tsx

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -277,7 +277,7 @@ function RegisterPageClient() {
               id='reason'
               rows={3}
               className='block w-full rounded-lg border-0 py-3 px-4 text-gray-900 dark:text-gray-100 shadow-sm ring-1 ring-white/60 dark:ring-white/20 placeholder:text-gray-500 dark:placeholder:text-gray-400 focus:ring-2 focus:ring-green-500 focus:outline-none sm:text-base bg-white/60 dark:bg-zinc-800/60 backdrop-blur'
-              placeholder='请简要说明注册理由（开启审核时填写更容易通过）'
+              placeholder='请简要说明注册理由（填写暗号可以更快通过哟~）'
               maxLength={200}
               value={reason}
               onChange={(e) => setReason(e.target.value)}
