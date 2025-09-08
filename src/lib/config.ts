c5216fa6a0ee94c052df2843972245641671741d
joyce677 <112773487+joyce677@users.noreply.github.com>
2025-09-08 13:26:43 +0800

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -313,11 +313,16 @@ export async function getConfig(): Promise<AdminConfig> {
 
   // db 中无配置，执行一次初始化
   if (!adminConfig) {
-    adminConfig = await getInitConfig("");
+    adminConfig = await getInitConfig('');
+    adminConfig = configSelfCheck(adminConfig);
+    // 只有在初始化时才保存
+    await db.saveAdminConfig(adminConfig);
+  } else {
+    // 对现有配置进行检查，但不立即保存
+    adminConfig = configSelfCheck(adminConfig);
   }
-  adminConfig = configSelfCheck(adminConfig);
+
   cachedConfig = adminConfig;
-  db.saveAdminConfig(cachedConfig);
   return cachedConfig;
 }
 
