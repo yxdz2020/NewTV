301aecb4cc1958ff2dec723a472115939b4413fe
joyce677 <112773487+joyce677@users.noreply.github.com>
2025-09-13 13:36:44 +0800

    Update page.tsx

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -2404,8 +2404,8 @@ function PlayPageClient() {
             /* 全屏模式下弹幕发射器宽度控制 */
             .art-fullscreen .artplayer-plugin-danmuku .apd-emitter,
             .art-fullscreen-web .artplayer-plugin-danmuku .apd-emitter {
-              width: 210px !important;
-              max-width: 210px !important;
+              width: 235px !important;
+              max-width: 235px !important;
             }
             
             .art-fullscreen .artplayer-plugin-danmuku .apd-emitter input,
