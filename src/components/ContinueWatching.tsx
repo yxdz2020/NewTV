e1778bced3d6c5407fecd4b1dc8104affbe4eabc
joyce677 <112773487+joyce677@users.noreply.github.com>
2025-09-14 16:40:26 +0800

    test

--- src/components/ContinueWatching.tsx
+++ src/components/ContinueWatching.tsx
@@ -109,7 +109,7 @@ export default function ContinueWatching({ className }: ContinueWatchingProps) {
             Array.from({ length: 6 }).map((_, index) => (
               <div
                 key={index}
-                className='min-w-[96px] w-24 sm:min-w-[180px] sm:w-44'
+                className='min-w-[115px] w-[115px] sm:min-w-[180px] sm:w-44'
               >
                 <div className='relative aspect-[2/3] w-full overflow-hidden rounded-lg bg-gray-200 animate-pulse dark:bg-gray-800'>
                   <div className='absolute inset-0 bg-gray-300 dark:bg-gray-700'></div>
@@ -124,7 +124,7 @@ export default function ContinueWatching({ className }: ContinueWatchingProps) {
               return (
                 <div
                   key={record.key}
-                  className='min-w-[96px] w-24 sm:min-w-[180px] sm:w-44'
+                  className='min-w-[115px] w-[115px] sm:min-w-[180px] sm:w-44'
                 >
                   <VideoCard
                     id={id}
